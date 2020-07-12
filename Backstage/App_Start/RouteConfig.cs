3bd9fcaa6926d4656539bc3137dbe1da5187bd2c
2opex <jerry1003tw@gmail.com>
2020-07-12 12:07:24 +0800

    Page Route

--- Backstage/App_Start/RouteConfig.cs
+++ Backstage/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Backstage
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Page",
+                url: "{controller}/{action}/{Page}",
+                defaults: new { controller = "Albums", action = "Index", Page = UrlParameter.Optional }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
